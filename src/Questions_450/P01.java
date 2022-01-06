3eaeb330955ce6b3af7733e0391c11835b3e7d07
Shruti Sharma <72591283+shrutiisharma@users.noreply.github.com>
2022-01-06 12:34:57 +0530

    Reverse a string

---
+++ src/Questions_450/P01.java
@@ -0,0 +1,53 @@
+//REVERSE A STRING
+
+package Questions_450;// { Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class P01
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        //method 1
+        char ch;
+        String revStr = "";
+
+        for(int i = 0; i < str.length(); i++){
+            ch = str.charAt(i);
+            revStr = ch + revStr;
+        }
+
+        return revStr;
+
+        /*
+        //method 2
+        StringBuilder revStr = new StringBuilder();
+        revStr.append(str);
+        revStr.reverse();
+        return String.valueOf(revStr);
+         */
+    }
+}
